dcfa9cd86688c04d168536b703264ddde93af06b
Daniel Saggau <53872337+danielsaggau@users.noreply.github.com>
2022-06-30 21:55:21 +0200

    Update train_ecthr_longformer.sh

--- Longformer/train_ecthr_longformer.sh
+++ Longformer/train_ecthr_longformer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #normal cpu stuff: allocate cpus, memory
-#SBATCH --ntasks=1 --cpus-per-task=8 --partition=mcml-dgx-a100-40x8
+#SBATCH --ntasks=1 --gpus-per-task=8 --partition=mcml-dgx-a100-40x8
 #SBATCH -p gpu --gres=gpu:1 --qos=mcml --mem=16GB
 #SBATCH --time=60:00:00
 #SBATCH --output=experiments-longformer-ecthr
