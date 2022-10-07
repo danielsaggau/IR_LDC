a6619f00c62738dbcf62038467f0b12ff6704845
Daniel Saggau <53872337+danielsaggau@users.noreply.github.com>
2022-10-07 17:37:08 +0200

    Create lbert_siwss_class.sh

---
+++ model/Supervised Learning/Scripts/lbert_siwss_class.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#normal cpu stuff: allocate cpus, memory
+#SBATCH --ntasks=1 --cpus-per-task=8 --partition=mcml-dgx-a100-40x8
+#SBATCH --qos=mcml --mem=16GB
+#SBATCH --time=1-00:00:00
+#SBATCH --output=experiments-longformer-ecthr
+#SBATCH --job-name=lexglue-longformer-ecthr
+
+#your script, in this case: write the hostname and the ids of the chosen gpus.
+#hostname
+echo $CUDA_VISIBLE_DEVICES
+export PYTHONPATH=.
+MODEL_PATH='models/lbert_swiss'
+
+python swiss_lbert.py --task swiss \
+--model_name_or_path ${MODEL_PATH} \
+        --do_lower_case 1 \
+        --output_dir  logs/${MODEL_PATH}-swiss \
+        --do_train \
+        --do_eval \
+        --do_pred \
+        --overwrite_output_dir \
+        --load_best_model_at_end \
+        --metric_for_best_model micro-f1 \
+        --greater_is_better 1 \
+        --evaluation_strategy  epoch \
+        --save_strategy epoch \
+        --save_total_limit  5 \
+        --num_train_epochs  20 \
+        --learning_rate 3e-5 \
+        --per_device_train_batch_size 6 \
+        --per_device_eval_batch_size  6 \
+        --fp16 \
+        --fp16_full_eval \
+        --max_seq_length  512
