5eaba5b22962451e3a93e8531feb7253bc332858
Daniel Saggau <53872337+danielsaggau@users.noreply.github.com>
2022-12-06 17:55:03 +0100

    Update run_ecthr.sh

--- model/ECTHR/run_ecthr.sh
+++ model/ECTHR/run_ecthr.sh
@@ -13,17 +13,22 @@
 echo $CUDA_VISIBLE_DEVICES
 export PYTHONPATH=.
 wandb login fd6f7deb3126d40be9abf77ee753bf45f00e2a9a
-python IR_LDC/model/ECTHR/ecthr_clean.py \
-        --output_dir  logs/echtr_b \
-        --overwrite_output_dir \
-        --load_best_model_at_end \
-        --metric_for_best_model micro-f1 \
-        --greater_is_better 1 \
-        --evaluation_strategy  epoch \
-        --save_strategy epoch \
-        --save_total_limit  5 \
-        --num_train_epochs  20 \
-        --learning_rate 5e-5 \
-        --per_device_train_batch_size 6 \
-        --per_device_eval_batch_size  6 \
-        --fp16 \
+!python /content/IR_LDC/model/ECTHR/bregman_ecthr.py \
+    --output_dir logs/output_1 \
+    --model_name 'danielsaggau/bregman_ecthrb_k_10_ep1' \
+    --load_best_model_at_end \
+    --model_type 'max' \
+    --overwrite_output_dir \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --learning_rate 5e-5 \
+    --per_device_train_batch_size 6 \
+    --per_device_eval_batch_size 6 \
+    --weight_decay 0.01 \
+    #--fp16 \
+    --num_train_epochs 10 \
+    --gradient_accumulation_steps 1 \
+    --metric_for_best_model "f1-micro" \
+    --greater_is_better 1 \
+    --report_to 'wandb' \
+    --model_type 'ecthr_breg_test'
