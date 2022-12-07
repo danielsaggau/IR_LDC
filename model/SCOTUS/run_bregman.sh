8789e65ba0a710cc80f6b153f9ee3fdfb9a1e488
Daniel Saggau <53872337+danielsaggau@users.noreply.github.com>
2022-12-07 17:53:01 +0100

    Update and rename run_bregman.py to run_bregman.sh

--- model/SCOTUS/run_bregman.sh
+++ model/SCOTUS/run_bregman.sh
@@ -3,11 +3,11 @@
 #SBATCH --ntasks=1 --gres=gpu:1 --partition=mcml-dgx-a100-40x8
 #SBATCH --qos=mcml --mem=16GB
 #SBATCH --time=0-07:00:00
-#SBATCH --e scotus.err
-#SBATCH --output=experiments-longformer-ecthr
-#SBATCH --job-name=lexglue-longformer-ecthr
+#SBATCH --error scotus.err
+#SBATCH --output=experiments-bregman
+#SBATCH --job-name=bregman-scotus-100k
 #SBATCH --mail-user=daniel.saggau@gmail.com
 #SBATCH --mail-type=ALL
 
 python -c "from huggingface_hub.hf_api import HfFolder; HfFolder.save_token('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')" 
-python /content/IR_LDC/model/SCOTUS/simcse_bregman_scotus_k100.py
+python /content/IR_LDC/model/SCOTUS/simcse_bregman_scotus.py
