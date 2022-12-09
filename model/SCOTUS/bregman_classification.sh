0841076cab4bf49d864884b8f8b92ae6951c0598
Daniel Saggau <53872337+danielsaggau@users.noreply.github.com>
2022-12-09 21:44:02 +0100

    Update bregman_classification.sh

--- model/SCOTUS/bregman_classification.sh
+++ model/SCOTUS/bregman_classification.sh
@@ -3,13 +3,14 @@
 #SBATCH --ntasks=1 --gres=gpu:1 --partition=mcml-dgx-a100-40x8
 #SBATCH --qos=mcml --mem=16GB
 #SBATCH --time=0-07:00:00
-#SBATCH --e scotus.err
+#SBATCH --error scotus.err
 #SBATCH --output=bregman_classification
 #SBATCH --job-name=bregmanclassification
 #SBATCH --mail-user=daniel.saggau@gmail.com
 #SBATCH --mail-type=ALL
+GPU_NUMBER=1
 wandb login fd6f7deb3126d40be9abf77ee753bf45f00e2a9a
-python bregman_scotus_classification.py \
+CUDA_VISIBLE_DEVICES=${GPU_NUMBER} python bregman_scotus_classification.py \
     --output_dir logs/output_1 \
     --model_name 'danielsaggau/bregman_scotus_k8_ep10' \
     --model_type 'mean' \
